b6f88abc3f0d46f192a57c95b5ce84327578f795
Raphael Bernardo <raphaelbgr@gmail.com>
2020-06-21 19:06:14 -0300

    + Included splash screen

--- app/src/main/java/com/stormbreakerstudios/android/challenge/BaseActivity.kt
+++ app/src/main/java/com/stormbreakerstudios/android/challenge/BaseActivity.kt
@@ -1,10 +1,5 @@
 package com.stormbreakerstudios.android.challenge
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-abstract class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+abstract class BaseActivity : AppCompatActivity()
--- app/src/main/java/com/stormbreakerstudios/android/challenge/SplashActivity.kt
+++ app/src/main/java/com/stormbreakerstudios/android/challenge/SplashActivity.kt
@@ -1,10 +1,3 @@
 package com.stormbreakerstudios.android.challenge
 
-import android.os.Bundle
-
-class SplashActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class SplashActivity : BaseActivity()
