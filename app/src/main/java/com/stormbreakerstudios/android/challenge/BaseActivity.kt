d23633f386b9d7e329de4793840f90ff40bfead0
Raphael Bernardo <raphaelbgr@gmail.com>
2020-06-21 18:44:36 -0300

    + Initial project commit

---
+++ app/src/main/java/com/stormbreakerstudios/android/challenge/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.stormbreakerstudios.android.challenge
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
