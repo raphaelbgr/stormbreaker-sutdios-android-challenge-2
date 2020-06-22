78a5a40ef88c8e1954424603915bbde04ad050ef
Raphael Bernardo <raphaelbgr@gmail.com>
2020-06-21 21:37:18 -0300

    + Adds support for vectors in old android APIs

--- app/src/main/java/com/stormbreakerstudios/android/challenge/BaseActivity.kt
+++ app/src/main/java/com/stormbreakerstudios/android/challenge/BaseActivity.kt
@@ -1,12 +1,19 @@
 package com.stormbreakerstudios.android.challenge
 
 import android.os.Build
+import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 abstract class BaseActivity : AppCompatActivity() {
     val navigator: Navigator by lazy(LazyThreadSafetyMode.PUBLICATION) { Navigator() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
 
     fun setTransparentStatusBarForKitKatAndAbove() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
